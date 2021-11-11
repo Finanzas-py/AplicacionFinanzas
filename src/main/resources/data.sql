2dbd6b4e6532a29b6315f9b254d66f7062121d30
Kevin Levin <kevinortegacalcin@gmail.com>
2021-11-11 18:04:27 -0500

    .

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,7 @@ INSERT INTO company(compay_name) VALUES ('Empresa');
 INSERT INTO company(compay_name) VALUES ('BCP');
 INSERT INTO company(compay_name) VALUES ('Interbank');
 INSERT INTO company(compay_name) VALUES ('Continental');
+INSERT INTO company(compay_name) VALUES ('Ninguna');
 
 INSERT INTO type(name) VALUES ('Documento');
 INSERT INTO type(name) VALUES ('DNI');
