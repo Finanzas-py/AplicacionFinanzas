bb284cc6d10436a76be9cca82020e4464e3c15e7
Kevin Levin <kevinortegacalcin@gmail.com>
2021-11-13 15:09:31 -0500

    función registras gi,gf

--- src/main/java/pe/edu/upc/spring/controller/DocumentController.java
+++ src/main/java/pe/edu/upc/spring/controller/DocumentController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.sun.el.parser.ParseException;
 
 import pe.edu.upc.spring.model.Cost;
+import pe.edu.upc.spring.model.ReasonCf;
 import pe.edu.upc.spring.model.ReasonCi;
 import pe.edu.upc.spring.model.Users;
 import pe.edu.upc.spring.service.ICompanyService;
@@ -40,46 +41,61 @@ public class DocumentController {
 	@Autowired
 	private IReasonCfService iReasonCfService;
 
-	@Autowired
-	private ICostService iCostService;
 
-	private List<Cost> listCostCi = new ArrayList<Cost>();
+	private List<Cost> listCostCi;
+	private List<Cost> listCostCf;
 	private String idReasonCi;
-	
+	private String idReasonCf;
+
 	@RequestMapping("/irRegistrarFactura")
 	public String irPaginaRegistrar(Model model) {
 		model.addAttribute("user", userController.sessionUser);
-		model.addAttribute("listReasonCi",iReasonCiService.listReasonCi());
-		model.addAttribute("listReasonCf",iReasonCfService.listReasonCf());
-		model.addAttribute("cost",new Cost());
+		model.addAttribute("listReasonCi", iReasonCiService.listReasonCi());
+		model.addAttribute("listReasonCf", iReasonCfService.listReasonCf());
+		 listCostCi = null;
+		 listCostCi= new ArrayList<Cost>();
+		 listCostCf = null;
+		 listCostCf= new ArrayList<Cost>();
+		model.addAttribute("cost", new Cost());
 
 		return "factura";
 	}
-	
+
 	@RequestMapping("/iractualizarFactura")
 	public String iractualizarFactura(Model model) {
 		model.addAttribute("user", userController.sessionUser);
-		model.addAttribute("listReasonCi",iReasonCiService.listReasonCi());
-		model.addAttribute("listCostInitials",listCostCi);
-		model.addAttribute("cost",new Cost());
+		model.addAttribute("listReasonCi", iReasonCiService.listReasonCi());
+		model.addAttribute("listReasonCf", iReasonCfService.listReasonCf());
+		model.addAttribute("listCostInitials", listCostCi);
+		model.addAttribute("listCostFinals", listCostCf);
+
+		model.addAttribute("cost", new Cost());
 
 		return "factura";
 	}
-	
+
 	@RequestMapping("/registrarCostosIniciales")
 	public String registrarCostoIniciales(@ModelAttribute Cost objCost, BindingResult binRes, Model model)
 			throws ParseException {
-		 idReasonCi = String.valueOf(objCost.getReasonCi().getIdReasonCi());
-		 List<ReasonCi> prueba = iReasonCiService.SearchById(idReasonCi);
-		 objCost.setReasonCi(prueba.get(0));
+		idReasonCi = String.valueOf(objCost.getReasonCi().getIdReasonCi());
+		List<ReasonCi> prueba = iReasonCiService.SearchById(idReasonCi);
+		objCost.setReasonCi(prueba.get(0));
 		listCostCi.add(objCost);
-		
-			
-			
-			return "redirect:/document/iractualizarFactura";
-		
+
+		return "redirect:/document/iractualizarFactura";
+
 	}
+	
+	@RequestMapping("/registrarCostosFinales")
+	public String registrarCostoFinales(@ModelAttribute Cost objCost, BindingResult binRes, Model model)
+			throws ParseException {
+		idReasonCf = String.valueOf(objCost.getReasonCf().getIdReasonCf());
+		List<ReasonCf> prueba = iReasonCfService.SearchById(idReasonCf);
+		objCost.setReasonCf(prueba.get(0));
+		listCostCf.add(objCost);
 
+		return "redirect:/document/iractualizarFactura";
 
+	}
 
 }
